8b440913a42014ae7dd380689112feb3996d21bb
unknown <poojithadarshan2131@gmail.com>
2024-03-04 11:39:49 +0530

    final

---
+++ src/main/java/onetoonebim7/controller/PersonAadharCont.java
@@ -0,0 +1,34 @@
+package onetoonebim7.controller;
+
+import onetoonebim7.dao.AadharCardDao;
+import onetoonebim7.dao.PersonDao;
+import onetoonebim7.dto.AadharCard;
+import onetoonebim7.dto.Person;
+
+public class PersonAadharCont {
+	public static void main(String[] args) {
+		Person person = new Person();
+		person.setId(1);
+		person.setName("bhuvan");
+		person.setAddress("mysore");
+
+		AadharCard aadharCard = new AadharCard();
+		aadharCard.setId(101);
+		aadharCard.setName("bhuvnasudhan");
+		aadharCard.setAge(18);
+		aadharCard.setPerson(person);
+		
+		PersonDao dao=new PersonDao();
+		dao.savePerson(person);
+		AadharCardDao aadharCardDao=new AadharCardDao();
+		aadharCardDao.saveAadharCard(aadharCard);
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
---
+++ src/main/java/onetoonebim7/dao/AadharCardDao.java
@@ -0,0 +1,78 @@
+package onetoonebim7.dao;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+import onetoonebim7.dto.AadharCard;
+import onetoonebim7.dto.Person;
+
+public class AadharCardDao {
+	public EntityManager getEntityManager() {
+		return Persistence.createEntityManagerFactory("vinod").createEntityManager();
+	}
+	
+	public void saveAadharCard(AadharCard aadharCard) {
+		EntityManager entityManager=getEntityManager();
+		EntityTransaction entityTransaction=entityManager.getTransaction();
+		entityTransaction.begin();
+		entityManager.persist(aadharCard);
+		Person dbPerson=aadharCard.getPerson();
+		dbPerson.setAadharCard(aadharCard);
+		entityManager.merge(dbPerson);
+		entityTransaction.commit();
+	}
+	
+	public void updateAadharCard(int id,AadharCard aadharCard) {
+		EntityManager entityManager=getEntityManager();
+		AadharCard dbAadharCard=entityManager.find(AadharCard.class, id);
+		if(dbAadharCard!=null) {
+//			id is present so i can update the data
+			EntityTransaction entityTransaction=entityManager.getTransaction();
+			entityTransaction.begin();
+			
+			aadharCard.setId(id);
+			aadharCard.setPerson(dbAadharCard.getPerson());
+			entityManager.merge(aadharCard);
+			entityTransaction.commit();
+			
+		}else {
+			System.out.println("Sorry id is not present");
+		}
+	}
+	
+	public void findCard(int id) {
+		EntityManager entityManager=getEntityManager();
+		AadharCard dbAadharCard=entityManager.find(AadharCard.class, id);
+		if(dbAadharCard!=null) {
+//			id is present so i can update the data
+			System.out.println(dbAadharCard);
+		}else {
+			System.out.println("Sorry id is not present");
+		}
+	}
+	
+	
+	public void deleteCard(int id) {
+		EntityManager entityManager=getEntityManager();
+		AadharCard dbAadharCard=entityManager.find(AadharCard.class, id);
+		if(dbAadharCard!=null) {
+//			id is present so i can update the data
+			EntityTransaction entityTransaction=entityManager.getTransaction();
+			entityTransaction.begin();
+			entityManager.remove(dbAadharCard);
+			entityTransaction.commit();
+		}else {
+			System.out.println("Sorry id is not present");
+		}
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
